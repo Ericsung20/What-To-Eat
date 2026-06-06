859e3eb4261affa48370c3e343159cae4d8be39e
Eric Sung <79889748+Ericsung20@users.noreply.github.com>
2026-06-06 03:10:37 -0500

    Delete What-To-Eat-Clean directory

--- What-To-Eat-Clean/WhatToEat-Frontend/src/screens/DiningHall/styles/MealPane.styles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export const styles = StyleSheet.create({
-    container: {
-        paddingHorizontal: 14,
-        paddingTop: 10,
-        paddingBottom: 14,
-    },
-});
--- What-To-Eat-Clean/WhatToEat-Frontend/src/services/api.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = 'https://whattoeat-backend-wh34.onrender.com';
-export const DEFAULT_USER_ID = '2a63f492-8875-4d3c-9b72-e262c3293219';
